c38817f03fc5d22004a66403ad2b542fb6787005
zhoujunzhou <7unzhou@gmail.com>
2014-08-20 10:23:36 +0800

    add resource file;

---
+++ src/com/dkhs/portfolio/ui/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.dkhs.portfolio.ui;
+
+import com.dkhs.portfolio.R;
+
+import android.os.Bundle;
+
+public class SettingActivity extends ModelAcitivity {
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		super.onCreate(arg0);
+		setContentView(R.layout.activity_setting);
+		setBackTitle(R.string.setting);
+	}
+}
