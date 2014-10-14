11036684374a315087be6ec1423cc338fb7dbec3
zhoujunzhou <zhoujunzhou@dkhs.com>
2014-10-14 20:35:44 +0800

    Add crash log;Optimize ui view;

--- src/com/dkhs/portfolio/app/PortfolioApplication.java
+++ src/com/dkhs/portfolio/app/PortfolioApplication.java
@@ -25,49 +25,53 @@ import com.dkhs.portfolio.utils.PortfolioPreferenceManager;
  * @version 1.0
  */
 public class PortfolioApplication extends Application {
-	private static PortfolioApplication mInstance;
+    private static PortfolioApplication mInstance;
 
-	public static PortfolioApplication getInstance() {
-		return mInstance;
-	}
+    public static PortfolioApplication getInstance() {
+        return mInstance;
+    }
 
-	@Override
-	public void onCreate() {
-		// TODO Auto-generated method stub
-		super.onCreate();
-		mInstance = this;
-		if (!PortfolioPreferenceManager.hasLoadSearchStock()) {
-			LoadStockToDBService.requestDownload(this);
-		}
-	}
+    @Override
+    public void onCreate() {
+        // TODO Auto-generated method stub
+        super.onCreate();
+        mInstance = this;
+        if (!PortfolioPreferenceManager.hasLoadSearchStock()) {
+            LoadStockToDBService.requestDownload(this);
+        }
 
-	private List<Activity> lists = new ArrayList<Activity>();
+        // 注册crashHandler
+        CrashHandler crashHandler = CrashHandler.getInstance(getApplicationContext());
 
-	public void addActivity(Activity activity) {
-		lists.add(activity);
-	}
+    }
 
-	public void exitApp() {
-		if (lists.size() > 0) {
-			for (int i = 0; i < lists.size(); i++) {
-				Activity activity = lists.get(i);
-				if (activity != null)
-					activity.finish();
-			}
-		}
-	}
+    private List<Activity> lists = new ArrayList<Activity>();
 
-	public void clearActivities() {
-		if (lists.size() > 1) {
-			for (int i = 0; i < lists.size() - 1; i++) {
-				Activity activity = lists.get(i);
-				if (activity != null)
-					activity.finish();
-			}
-			Activity activity = lists.get(lists.size() - 1);
-			lists.clear();
-			lists.add(activity);
-		}
-	}
+    public void addActivity(Activity activity) {
+        lists.add(activity);
+    }
+
+    public void exitApp() {
+        if (lists.size() > 0) {
+            for (int i = 0; i < lists.size(); i++) {
+                Activity activity = lists.get(i);
+                if (activity != null)
+                    activity.finish();
+            }
+        }
+    }
+
+    public void clearActivities() {
+        if (lists.size() > 1) {
+            for (int i = 0; i < lists.size() - 1; i++) {
+                Activity activity = lists.get(i);
+                if (activity != null)
+                    activity.finish();
+            }
+            Activity activity = lists.get(lists.size() - 1);
+            lists.clear();
+            lists.add(activity);
+        }
+    }
 
 }
