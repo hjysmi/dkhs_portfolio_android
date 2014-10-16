4fdf17503e7a2b55c0c2fb20eeae1bab03d038da
weiting <huangweiting@dkhs.com>
2014-10-16 15:28:19 +0800

    字体大小

--- src/com/dkhs/portfolio/ui/SettingActivity.java
+++ src/com/dkhs/portfolio/ui/SettingActivity.java
@@ -1,28 +1,43 @@
 package com.dkhs.portfolio.ui;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 import com.dkhs.portfolio.R;
 import com.dkhs.portfolio.app.PortfolioApplication;
+import com.dkhs.portfolio.bean.CombinationBean;
 import com.dkhs.portfolio.bean.UserEntity;
 import com.dkhs.portfolio.common.GlobalParams;
+import com.dkhs.portfolio.engine.MyCombinationEngineImpl;
 import com.dkhs.portfolio.engine.UserEngineImpl;
+import com.dkhs.portfolio.net.DataParse;
 import com.dkhs.portfolio.net.ParseHttpListener;
+import com.google.gson.reflect.TypeToken;
 import com.lidroid.xutils.DbUtils;
 import com.lidroid.xutils.exception.DbException;
+import com.lidroid.xutils.util.LogUtils;
 
 public class SettingActivity extends ModelAcitivity implements OnClickListener{
 public static boolean isSetPassword = true;
+private LinearLayout settingLayoutGroup;
+private Context context;
 	
 	@SuppressLint("HandlerLeak")
 	private Handler handler = new Handler() {
@@ -39,9 +54,11 @@ public static boolean isSetPassword = true;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setting_layout);
+		context = this;
 		initViews();
 		setListener();
 		initData();
+		loadCombinationData();
 	}
 	
 	public void initData() {
@@ -80,6 +97,7 @@ public static boolean isSetPassword = true;
 	public void initViews() {
 		// TODO Auto-generated method stub
 		setTitle(R.string.setting);
+		settingLayoutGroup = (LinearLayout) findViewById(R.id.setting_layout_group);
 	}
 
 	@Override
@@ -129,4 +147,35 @@ public static boolean isSetPassword = true;
 			break;
 		}
 	}
+	public void createGroupShow(List<CombinationBean> lsit){
+		int i = 0;
+		for (CombinationBean combinationBean : lsit) {
+			LayoutInflater l = LayoutInflater.from(context);
+			View view = l.inflate(R.layout.setting_group_item, null);
+			Switch s = (Switch) view.findViewById(R.id.switch1);
+			s.setText(combinationBean.getName());
+			if(i++%2 == 0)
+				s.setChecked(true);
+			settingLayoutGroup.addView(view);
+		}
+	}
+	private void loadCombinationData() {
+        new MyCombinationEngineImpl().getCombinationList(new ParseHttpListener<List<CombinationBean>>() {
+
+            @Override
+            protected List<CombinationBean> parseDateTask(String jsonData) {
+                Type listType = new TypeToken<List<CombinationBean>>() {
+                }.getType();
+                List<CombinationBean> combinationList = DataParse.parseJsonList(jsonData, listType);
+
+                return combinationList;
+            }
+
+            @Override
+            protected void afterParseData(List<CombinationBean> dataList) {
+                createGroupShow(dataList);
+            }
+
+        }.setLoadingDialog(SettingActivity.this, R.string.loading));
+    }
 }
