de4f47f4f94755a159dfec40512d9e8fd97dc8d0
weiting <huangweiting@dkhs.com>
2014-11-19 15:05:52 +0800

    修改行情中心实现方式，添加左右滑动切换界面功能。

--- src/com/dkhs/portfolio/ui/MarketCenterActivity.java
+++ src/com/dkhs/portfolio/ui/MarketCenterActivity.java
@@ -1,11 +1,15 @@
 package com.dkhs.portfolio.ui;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Matrix;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +22,8 @@ import android.widget.TextView;
 
 import com.dkhs.portfolio.R;
 import com.dkhs.portfolio.ui.OptionalStockListActivity.RequestMarketTask;
+import com.dkhs.portfolio.ui.adapter.FragmentSelectAdapter;
+import com.dkhs.portfolio.ui.fragment.FragmentMarkerCenter;
 import com.dkhs.portfolio.ui.fragment.FragmentSelectStockFund;
 import com.dkhs.portfolio.ui.fragment.FragmentSelectStockFund.ViewType;
 /**
@@ -25,205 +31,21 @@ import com.dkhs.portfolio.ui.fragment.FragmentSelectStockFund.ViewType;
  * @author weiting
  *
  */
-public class MarketCenterActivity extends ModelAcitivity implements
-		OnClickListener {
-	private FragmentSelectStockFund loadDataListFragment;
-	private ImageView marketIconCursor;
-	private int bmpW;
-	private int offset;
-	private int currIndex = 1;
-	private TextView textInland;
-	private TextView textShangAndShen;
-	private LinearLayout marketLayoutUpanddown;
-	private boolean ace = true;
-	private ImageView marketIconUpDown;
-	private TextView marketTextIndex;
-	private TextView marketTextEdition;
-	private Timer mMarketTimer;
-	private static final long mPollRequestTime = 1000 * 5;
+public class MarketCenterActivity extends ModelAcitivity{
+	private LinearLayout layoutMarkerParent;
+	private Context context;
 	@Override
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_marketcenter);
 		setTitle(R.string.marketcenter_title);
-		replaceDataList();
-		initView();
-		InitImageView();
-	}
-
-	private void initView() {
-		marketIconCursor = (ImageView) findViewById(R.id.market_icon_cursor);
-		textInland = (TextView) findViewById(R.id.text1);
-		textShangAndShen = (TextView) findViewById(R.id.text2);
-		marketLayoutUpanddown = (LinearLayout) findViewById(R.id.market_layout_upanddown);
-		marketIconUpDown = (ImageView) findViewById(R.id.market_icon_up_down);
-		marketIconUpDown.setVisibility(View.GONE);
-		marketTextIndex = (TextView) findViewById(R.id.market_text_index);
-		marketTextEdition = (TextView) findViewById(R.id.market_text_edition);
-		textInland.setOnClickListener(this);
-		textShangAndShen.setOnClickListener(this);
-		marketLayoutUpanddown.setOnClickListener(this);
-		Button btn = getRightButton();
-		btn.setBackgroundResource(R.drawable.btn_search_select);
-		btn.setOnClickListener(this);
-	}
-
-	private void replaceDataList() {
-		// view_datalist
-		if (null == loadDataListFragment) {
-			loadDataListFragment = loadDataListFragment
-					.getStockFragment(ViewType.STOC_INDEX_MARKET_CURRENT);
-			
-		}
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.view_datalist, loadDataListFragment).commit();
-		
-	}
-	@Override
-    public void onResume() {
-
-        super.onResume();
-
-        if (mMarketTimer == null) {
-            mMarketTimer = new Timer(true);
-            mMarketTimer.schedule(new RequestMarketTask(), mPollRequestTime, mPollRequestTime);
-        }
-
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        if (mMarketTimer != null) {
-            mMarketTimer.cancel();
-            mMarketTimer = null;
-        }
-
-    }
-
-    public class RequestMarketTask extends TimerTask {
-
-        @Override
-        public void run() {
-
-            loadDataListFragment.refreshForMarker();
-        }
-    }
-	/**
-	 * 初始化动画
-	 */
-	private void InitImageView() {
-		
-		DisplayMetrics dm = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(dm);
-		bmpW = (int) (80* dm.densityDpi/160);// 获取图片宽度
-		int screenW = dm.widthPixels;// 获取分辨率宽度
-		
-		offset = (screenW / 2 - bmpW) / 2;// 计算偏移量
-		Matrix matrix = new Matrix();
-		matrix.postTranslate(offset, 0);
-		marketIconCursor.setImageMatrix(matrix);// 设置动画初始位置\
-		Animation animation = null;
-		animation = new TranslateAnimation(offset,offset , 0, 0);
-		animation.setFillAfter(true);// True:图片停在动画结束位置
-		animation.setDuration(300);
-		marketIconCursor.startAnimation(animation);
-		textInland.setTextColor(getResources().getColor(R.color.red));
-	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Animation animation = null;
-		switch (v.getId()) {
-		case R.id.text1:
-			if (currIndex == 2) {
-				animation = new TranslateAnimation(offset * 3 + bmpW,offset , 0, 0);
-				currIndex = 1;
-				textInland.setTextColor(getResources().getColor(R.color.red));
-				textShangAndShen.setTextColor(getResources().getColor(R.color.black));
-				animation.setFillAfter(true);// True:图片停在动画结束位置
-				animation.setDuration(300);
-				marketIconCursor.startAnimation(animation);
-				loadDataListFragment = loadDataListFragment
-						.getStockFragment(ViewType.STOC_INDEX_MARKET_CURRENT);
-				marketIconUpDown.setImageResource(R.drawable.market_icon_down);
-				marketIconUpDown.setVisibility(View.GONE);
-				getSupportFragmentManager().beginTransaction()
-				.replace(R.id.view_datalist, loadDataListFragment).commit();
-				 ace = true;
-				 marketTextIndex.setText("股指");
-				 marketTextEdition.setText("最新");
-			}else{
-				currIndex = 1;
-				textInland.setTextColor(getResources().getColor(R.color.red));
-				textShangAndShen.setTextColor(getResources().getColor(R.color.black));
-				loadDataListFragment = loadDataListFragment
-						.getStockFragment(ViewType.STOC_INDEX_MARKET_CURRENT);
-				marketIconUpDown.setImageResource(R.drawable.market_icon_down);
-				marketIconUpDown.setVisibility(View.GONE);
-				getSupportFragmentManager().beginTransaction()
-				.replace(R.id.view_datalist, loadDataListFragment).commit();
-				 ace = false;
-				 marketTextIndex.setText("股指");
-				 marketTextEdition.setText("最新");
-			}
-			break;
-		case R.id.text2:
-			if (currIndex == 1) {
-				marketIconUpDown.setVisibility(View.VISIBLE);
-				animation = new TranslateAnimation(offset,offset* 3 + bmpW,0, 0);
-				currIndex = 2;
-				textInland.setTextColor(getResources().getColor(R.color.black));
-				textShangAndShen.setTextColor(getResources().getColor(R.color.red));
-				animation.setFillAfter(true);// True:图片停在动画结束位置
-				animation.setDuration(300);
-				marketIconCursor.startAnimation(animation);
-				loadDataListFragment = loadDataListFragment
-						.getStockFragment(ViewType.STOC_INDEX_POSITION);
-				marketIconUpDown.setImageResource(R.drawable.market_icon_down);
-				getSupportFragmentManager().beginTransaction()
-				.replace(R.id.view_datalist, loadDataListFragment).commit();
-				 ace = false;
-				 marketTextIndex.setText("股票");
-				 marketTextEdition.setText("最新");
-			}
-			break;
-		case R.id.market_layout_upanddown:
-			marketIconUpDown.setVisibility(View.VISIBLE);
-			if(ace){
-				if(currIndex == 1){
-				loadDataListFragment = loadDataListFragment
-						.getStockFragment(ViewType.STOC_INDEX_MARKET);
-				marketIconUpDown.setImageResource(R.drawable.market_icon_down);
-				}else{
-					loadDataListFragment = loadDataListFragment
-							.getStockFragment(ViewType.STOC_INDEX_POSITION);
-					marketIconUpDown.setImageResource(R.drawable.market_icon_down);
-				}
-				ace = false;
-			}else{
-				if(currIndex == 1){
-				loadDataListFragment = loadDataListFragment
-						.getStockFragment(ViewType.STOC_INDEX_MARKET_ACE);
-				marketIconUpDown.setImageResource(R.drawable.market_icon_up);
-				}else{
-					loadDataListFragment = loadDataListFragment
-							.getStockFragment(ViewType.STOC_INDEX_POSITION_ACE);
-					marketIconUpDown.setImageResource(R.drawable.market_icon_up);
-				}
-				ace = true;
-			}
-			getSupportFragmentManager().beginTransaction()
-					.replace(R.id.view_datalist, loadDataListFragment).commit();
-			break;
-		case R.id.btn_right:
-			Intent intent = new Intent(this, SelectAddOptionalActivity.class);
-            startActivity(intent);
-			break;
-		}
-		
+		layoutMarkerParent = (LinearLayout) findViewById(R.id.layout_marker_parenst);
+		context = this;
+		String[] name = new String[]{"国内指数","沪深行情"};
+		List<Fragment> frag = new ArrayList<Fragment>();
+		frag.add(FragmentMarkerCenter.initFrag(FragmentMarkerCenter.INSIDE_COUNT));
+		frag.add(FragmentMarkerCenter.initFrag(FragmentMarkerCenter.SHEN_HU));
+		new FragmentSelectAdapter(context, name, frag, layoutMarkerParent, getSupportFragmentManager());
 	}
 }
