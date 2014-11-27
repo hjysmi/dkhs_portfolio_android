e1f21fea5829bf976571ed544bfa298ea5c30586
weiting <huangweiting@dkhs.com>
2014-11-27 20:28:10 +0800

    主页公告研报UI修改。

--- src/com/dkhs/portfolio/ui/adapter/OptionMarketAdapter.java
+++ src/com/dkhs/portfolio/ui/adapter/OptionMarketAdapter.java
@@ -6,8 +6,15 @@ import java.util.Date;
 import java.util.List;
 
 import android.content.Context;
+import android.support.v4.view.MarginLayoutParamsCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
@@ -19,6 +26,7 @@ public class OptionMarketAdapter extends BaseAdapter{
 	private Context mContext;
 	private List<OptionNewsBean> mDataList;
 	private OptionNewsBean mOptionNewsBean;
+	private ViewHodler viewHolder = null;
 	public OptionMarketAdapter(Context mContext,List<OptionNewsBean> mDataList){
 		this.mContext = mContext;
 		this.mDataList = mDataList;
@@ -47,22 +55,23 @@ public class OptionMarketAdapter extends BaseAdapter{
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		ViewHodler viewHolder = null;
 		try {
 			mOptionNewsBean = mDataList.get(position);
-			if (convertView == null) {
 			    viewHolder = new ViewHodler();
+			    final TextView tv;
 			    convertView = View.inflate(mContext, R.layout.adapter_opition_news, null);
-			    viewHolder.tvTextName = (TextView) convertView.findViewById(R.id.adapter_market_title);
+			    tv = (TextView) convertView.findViewById(R.id.adapter_market_title);
 			    viewHolder.tvTextNameNum = (TextView) convertView.findViewById(R.id.adapter_market_title_num);
 			    viewHolder.tvTextDate = (TextView) convertView.findViewById(R.id.option_news_text_date);
+			    TextView texts = (TextView) convertView.findViewById(R.id.zhengquan);
 			    convertView.setTag(viewHolder);
-			} else {
-			    viewHolder = (ViewHodler) convertView.getTag();
-			}
-			viewHolder.tvTextName.setText(mOptionNewsBean.getTitle());
+			tv.setText(mOptionNewsBean.getTitle());
+			ViewTreeObserver observer = tv.getViewTreeObserver();
 			viewHolder.tvTextNameNum.setText(mOptionNewsBean.getSymbols().get(0).getAbbrName());
 			Calendar old = TimeUtils.toCalendar(mOptionNewsBean.getPublish());
+			if(null != mOptionNewsBean.getSource()){
+				texts.setText(mOptionNewsBean.getSource().getTitle());
+			}
 			if(TimeUtils.compareTime(old)){
 				viewHolder.tvTextDate.setText((old.get(Calendar.HOUR_OF_DAY) < 10 ? ("0" + old.get(Calendar.HOUR_OF_DAY)) : old.get(Calendar.HOUR_OF_DAY) ) + ":" + (old.get(Calendar.MINUTE) < 10 ? ("0" + old.get(Calendar.MINUTE)) : old.get(Calendar.MINUTE)));
 			}else{
@@ -70,6 +79,28 @@ public class OptionMarketAdapter extends BaseAdapter{
 				viewHolder.tvTextDate.setText((t < 10 ? ("0" + t) : t) +"-" + (old.get(Calendar.DAY_OF_MONTH) < 10 ? ("0" + old.get(Calendar.DAY_OF_MONTH)) :old.get(Calendar.DAY_OF_MONTH)));
 			}
 			//viewHolder.tvTextDate.setText(mOptionNewsBean.getPublish().replace("T", " ").substring(0, mOptionNewsBean.getCreatedTime().length()-6) + "00");
+			observer.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+				@Override
+				public void onGlobalLayout() {
+				ViewTreeObserver obs = tv.getViewTreeObserver();
+				obs.removeGlobalOnLayoutListener(this);
+				if(tv.getLineCount() > 1){
+					String text;
+					try {
+						int lineEndIndex = tv.getLayout().getLineEnd(0); //设置第六行打省略号
+						if(tv.getLayout().getLineEnd(1) > (lineEndIndex / 2)){
+							text = tv.getText().subSequence(0, lineEndIndex*3/2) +"...";
+							tv.setText(text);
+						}else{
+							tv.setText(mOptionNewsBean.getTitle());
+						}
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				}
+				});
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
