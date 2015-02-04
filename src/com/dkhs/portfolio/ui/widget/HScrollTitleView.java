3ecbb54622553c97eea3f14495f9237f92081a88
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-02-04 16:38:15 +0800

    修复：修复：组合详情页，相关信息->个股公告，进入二级列表，上拉加载;

--- src/com/dkhs/portfolio/ui/widget/HScrollTitleView.java
+++ src/com/dkhs/portfolio/ui/widget/HScrollTitleView.java
@@ -157,9 +157,9 @@ public class HScrollTitleView extends FrameLayout implements AnimationListener {
             tv.setTextColor(getContext().getResources().getColor(R.color.black));
             tv.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     getContext().getResources().getDimensionPixelSize(R.dimen.title_tab_text_size));
-            tv.setPadding(0, 5, 0, 5);
+            // tv.setPadding(0, 5, 0, 5);
             tv.setGravity(Gravity.CENTER);
-            tv.setLayoutParams(new LinearLayout.LayoutParams(indiatorWidth, LayoutParams.WRAP_CONTENT, 1.0f));
+            tv.setLayoutParams(new LinearLayout.LayoutParams(indiatorWidth, LayoutParams.MATCH_PARENT, 1.0f));
             tv.setText(nameList[i]);
             tv.setOnClickListener(new OnItemListener(i));
             ll.addView(tv);
