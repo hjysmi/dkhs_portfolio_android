59697cf0e7b772009a251c1361dec2a5a3b73781
weiting <huangweiting@dkhs.com>
2015-02-12 17:55:11 +0800

    域名改为IP地址.

--- src/com/dkhs/portfolio/net/DKHSUrl.java
+++ src/com/dkhs/portfolio/net/DKHSUrl.java
@@ -18,14 +18,14 @@ package com.dkhs.portfolio.net;
 public class DKHSUrl {
 
     // 测试
-    public static final String BASE_TEST_URL = "http://dev.dkhs.com:8030";
+    public static final String BASE_TEST_URL = "http://121.41.25.170:8030";
 
     // 开发
-    public static final String BASE_DEV_URL = "http://dev.dkhs.com:8010";
+    public static final String BASE_DEV_URL = "http://121.41.25.170:8010";
     // 正式
     public static final String BASE_DEV_MAIN = "https://www.dkhs.com";
     // 服务器
-    public static final String BASE_DEV_TAG = "http://dev.dkhs.com:8050";
+    public static final String BASE_DEV_TAG = "http://121.41.25.170:8050";
 
     // public static final String BASE_URL = "http://192.168.107.251:8010";
     // public static final String BASE_URL = "http://58.23.5.117:8030";
