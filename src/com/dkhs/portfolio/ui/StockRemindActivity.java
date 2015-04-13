ef9cb06c8eb1e88c4fd71d849f0f0905e5fe5e5f
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-04-13 17:22:00 +0800

    添加：提醒设置界面；

--- src/com/dkhs/portfolio/ui/StockRemindActivity.java
+++ src/com/dkhs/portfolio/ui/StockRemindActivity.java
@@ -8,18 +8,21 @@
  */
 package com.dkhs.portfolio.ui;
 
-import java.io.Serializable;
-import java.util.List;
-
-import com.dkhs.portfolio.R;
-import com.dkhs.portfolio.bean.SelectStockBean;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.dkhs.portfolio.R;
+import com.dkhs.portfolio.bean.SelectStockBean;
+import com.dkhs.portfolio.utils.StringFromatUtils;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 
 /**
  * @ClassName StockRemindActivity
@@ -33,7 +36,61 @@ public class StockRemindActivity extends ModelAcitivity implements OnClickListen
     public static final String ARGUMENT_STOCK = "agrument_stock";
     private Button btnRight;
     private SelectStockBean mStockBean;
-    
+
+    private boolean isCombinationSetting = false;
+
+    @ViewInject(R.id.tv_stock_name)
+    private TextView tvStockName;
+    @ViewInject(R.id.tv_stock_num)
+    private TextView tvStockCode;
+    @ViewInject(R.id.tv_percent)
+    private TextView tvPercent;
+    @ViewInject(R.id.tv_price)
+    private TextView tvPrice;
+
+    // 涨跌幅
+    @ViewInject(R.id.tv_priceup_text)
+    private TextView tvUpText;
+    @ViewInject(R.id.tv_pricedown_text)
+    private TextView tvDownText;
+
+    // 股价涨跌幅提示
+    @ViewInject(R.id.tv_priceup_tip)
+    private TextView tvUpTip;
+    @ViewInject(R.id.tv_pricedown_tip)
+    private TextView tvDownTip;
+    @ViewInject(R.id.tv_daypercent_tip)
+    private TextView tvDayPercentTip;
+
+    // 股价涨跌幅，switch
+    @ViewInject(R.id.sw_price_up)
+    private Switch swPriceUp;
+    @ViewInject(R.id.sw_price_down)
+    private Switch swPriceDown;
+    @ViewInject(R.id.sw_day_percent)
+    private Switch swDayPercent;
+
+    // 股价涨跌幅输入
+    @ViewInject(R.id.et_priceup)
+    private EditText etPriceUp;
+    @ViewInject(R.id.et_pricedown)
+    private EditText etPriceDown;
+    @ViewInject(R.id.et_daypercent)
+    private EditText etDayPercent;
+
+    @ViewInject(R.id.rl_adjust_remind)
+    private View viewAdjustRemind;
+    @ViewInject(R.id.rl_notice_remind)
+    private View viewNoticeRemind;
+    @ViewInject(R.id.rl_yanbao_remind)
+    private View viewYanbaoRemind;
+
+    @ViewInject(R.id.sw_adjust_remind)
+    private Switch swAdjustRemind;
+    @ViewInject(R.id.sw_notice_remind)
+    private Switch swNoticeRemind;
+    @ViewInject(R.id.sw_yanbao_remind)
+    private Switch swYanbaoRemind;
 
     public static Intent newStockIntent(Context context, SelectStockBean stock) {
         Intent intent = new Intent(context, StockRemindActivity.class);
@@ -46,11 +103,11 @@ public class StockRemindActivity extends ModelAcitivity implements OnClickListen
         super.onCreate(arg0);
         setTitle(R.string.title_remind);
         setContentView(R.layout.activity_stock_remind);
+        ViewUtils.inject(this);
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             handleExtras(extras);
         }
-
         initView();
     }
 
@@ -63,6 +120,34 @@ public class StockRemindActivity extends ModelAcitivity implements OnClickListen
         btnRight.setOnClickListener(this);
         btnRight.setText(R.string.finish);
 
+        if (isCombinationSetting) {
+            setCombinationStyle();
+        } else {
+            setStockStyle();
+        }
+
+    }
+
+    private void setCombinationStyle() {
+        tvStockCode.setVisibility(View.INVISIBLE);
+        tvUpText.setText(R.string.combination_price_up_text);
+        tvDownText.setText(R.string.combination_price_down_text);
+        viewAdjustRemind.setVisibility(View.VISIBLE);
+        viewNoticeRemind.setVisibility(View.GONE);
+        viewYanbaoRemind.setVisibility(View.GONE);
+
+    }
+
+    private void setStockStyle() {
+        if (null != mStockBean) {
+            tvStockName.setText(mStockBean.getName());
+            tvStockCode.setText(mStockBean.getCode());
+            String perText = getString(R.string.format_percent,
+                    StringFromatUtils.get2PointPercent(mStockBean.getPercentage()));
+            tvPercent.setText(perText);
+            perText = getString(R.string.format_stock_price, mStockBean.getCurrentValue());
+            tvPrice.setText(perText);
+        }
     }
 
     @Override
