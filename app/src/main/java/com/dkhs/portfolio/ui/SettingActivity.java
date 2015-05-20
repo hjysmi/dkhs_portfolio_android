f469e3a4b3bf225fd72953bd18027c11f372d554
曾伟明 <zengweiming@dkhs.com>
2015-05-20 14:26:33 +0800

    主页界面的自建改为自选 个人设置界面多条线

--- app/src/main/java/com/dkhs/portfolio/ui/SettingActivity.java
+++ app/src/main/java/com/dkhs/portfolio/ui/SettingActivity.java
@@ -189,8 +189,11 @@ public class SettingActivity extends ModelAcitivity implements OnClickListener {
             findViewById(R.id.rl_aboutus).setVisibility(View.GONE);
             findViewById(R.id.btn_exit).setVisibility(View.GONE);
             findViewById(R.id.setting_layout_bound).setVisibility(View.GONE);
+            findViewById(R.id.line4).setVisibility(View.GONE);
             findViewById(R.id.line5).setVisibility(View.GONE);
             findViewById(R.id.line6).setVisibility(View.GONE);
+            findViewById(R.id.line7).setVisibility(View.GONE);
+            findViewById(R.id.line8).setVisibility(View.GONE);
 //            findViewById(R.id.line_tx). findViewById(R.id.line).setVisibility(View.GONE);
         }else{
 
