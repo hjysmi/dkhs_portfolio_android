89a1449066c33c58bab6e7a4eb5d24c5ecfe3a99
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-06-04 16:41:28 +0800

    修改：移除版本

--- app/src/main/java/com/dkhs/portfolio/ui/fragment/MainOptionalFragment.java
+++ app/src/main/java/com/dkhs/portfolio/ui/fragment/MainOptionalFragment.java
@@ -12,7 +12,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
