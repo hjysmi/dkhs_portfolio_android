6476a716bbf6203da07d90848076e5d6c3da888d
曾伟明 <zengweiming@dkhs.com>
2015-07-07 13:38:10 +0800

    去掉多余代码

--- app/src/main/java/com/dkhs/portfolio/ui/MainActivity.java
+++ app/src/main/java/com/dkhs/portfolio/ui/MainActivity.java
@@ -54,35 +54,6 @@ public class MainActivity extends BaseActivity {
 
     private MessageHandler handler;
 
-    public static String getDeviceInfo(Context context) {
-        try{
-            org.json.JSONObject json = new org.json.JSONObject();
-            android.telephony.TelephonyManager tm = (android.telephony.TelephonyManager) context
-                    .getSystemService(Context.TELEPHONY_SERVICE);
-
-            String device_id = tm.getDeviceId();
-
-            android.net.wifi.WifiManager wifi = (android.net.wifi.WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-
-            String mac = wifi.getConnectionInfo().getMacAddress();
-            json.put("mac", mac);
-
-            if( TextUtils.isEmpty(device_id) ){
-                device_id = mac;
-            }
-
-            if( TextUtils.isEmpty(device_id) ){
-                device_id = android.provider.Settings.Secure.getString(context.getContentResolver(),android.provider.Settings.Secure.ANDROID_ID);
-            }
-
-            json.put("device_id", device_id);
-
-            return json.toString();
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
-    }
 
 
 
@@ -91,7 +62,6 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         // 模拟堆栈管理activity
         PortfolioApplication.getInstance().addActivity(this);
-        LogUtils.e(getDeviceInfo(this));
         // setTheme(android.R.style.Theme_Light_NoTitleBar);
         // PortfolioApplication.getInstance().addActivity(this);
         handler = new MessageHandler(this);
