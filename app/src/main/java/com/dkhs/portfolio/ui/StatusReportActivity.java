34d60ed6920a307c68b92d2ef2226cf2259a3413
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-07-28 09:43:00 +0800

    添加举报界面

---
+++ app/src/main/java/com/dkhs/portfolio/ui/StatusReportActivity.java
@@ -0,0 +1,19 @@
+package com.dkhs.portfolio.ui;
+
+import android.os.Bundle;
+
+import com.dkhs.portfolio.R;
+
+/**
+ * Created by zjz on 2015/7/27.
+ */
+public class StatusReportActivity extends ModelAcitivity {
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setTitle(R.string.title_status_report);
+    }
+}
+
+
