9de8bea3f61f7fecd42456a4a48ed1b2d437da74
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-08-11 16:02:45 +0800

    修改：主界面（我的）界面修改

--- app/src/main/java/com/dkhs/portfolio/ui/adapter/UserInfoAdapter.java
+++ app/src/main/java/com/dkhs/portfolio/ui/adapter/UserInfoAdapter.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.dkhs.portfolio.R;
@@ -18,23 +22,35 @@ import com.dkhs.portfolio.bean.UserEntity;
 import com.dkhs.portfolio.common.GlobalParams;
 import com.dkhs.portfolio.engine.AdEngineImpl;
 import com.dkhs.portfolio.engine.UserEngineImpl;
+import com.dkhs.portfolio.net.DataParse;
+import com.dkhs.portfolio.net.ParseHttpListener;
 import com.dkhs.portfolio.net.SimpleParseHttpListener;
 import com.dkhs.portfolio.ui.FlowPackageActivity;
+import com.dkhs.portfolio.ui.FriendsOrFollowersActivity;
 import com.dkhs.portfolio.ui.InviteFriendsActivity;
 import com.dkhs.portfolio.ui.MyCombinationActivity;
 import com.dkhs.portfolio.ui.MyDraftActivity;
 import com.dkhs.portfolio.ui.ReplyActivity;
+import com.dkhs.portfolio.ui.UserHomePageActivity;
 import com.dkhs.portfolio.ui.UserTopicsActivity;
 import com.dkhs.portfolio.ui.messagecenter.MessageManager;
+import com.dkhs.portfolio.utils.PortfolioPreferenceManager;
+import com.dkhs.portfolio.utils.StringFromatUtils;
 import com.dkhs.portfolio.utils.UIUtils;
 import com.dkhs.widget.FlexibleDividerDecoration;
+import com.lidroid.xutils.BitmapUtils;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
 
 /**
  * Created by zjz on 2015/7/22.
  */
-public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHolder> implements
-        FlexibleDividerDecoration.SizeProvider {
+public class UserInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements FlexibleDividerDecoration.SizeProvider {
 
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
 
     private String[] titleTexts = PortfolioApplication.getInstance().getResources().getStringArray(R.array.user_info_title);
     private int[] iconRes;
@@ -56,7 +72,9 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
         iconRes = new int[len];
         for (int i = 0; i < len; i++) iconRes[i] = ar.getResourceId(i, 0);
         ar.recycle();
-        getInviteCode();
+        if (PortfolioApplication.hasUserLogin()) {
+            getInviteCode();
+        }
 
     }
 
@@ -72,6 +90,7 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
             protected void afterParseData(Object object) {
                 if (object != null) {
                     mInviteCode = ((ShareBean) object).getCode();
+                    Log.d("UserInfoAdatper", mInviteCode + " mInviteCode");
                     notifyDataSetChanged();
                 }
             }
@@ -79,45 +98,65 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = mLayoutInflater.inflate(R.layout.item_user_info, parent, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == TYPE_ITEM) {
+            view = mLayoutInflater.inflate(R.layout.item_user_info, parent, false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_HEADER) {
+            view = mLayoutInflater.inflate(R.layout.layout_userinfo_header, parent, false);
+            return new HeadViewHolder(view);
+        }
+
+        throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
+
+
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.tvTitle.setText(titleTexts[position]);
-        holder.tvTitle.setCompoundDrawablesWithIntrinsicBounds(iconRes[position],
-                0, 0, 0);
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clickPosition(position);
-            }
-        });
-        if (position >= titleTexts.length - 2) {
-            holder.tvTip.setVisibility(View.VISIBLE);
-            if (position == titleTexts.length - 1) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
 
-                holder.tvTip.setText(R.string.tip_flowpackage);
-            } else if (position == titleTexts.length - 2 && !TextUtils.isEmpty(mInviteCode)) {
-                holder.tvTip.setText(mContext.getString(R.string.tip_invite_code, mInviteCode));
-            }
-        } else {
-            holder.tvTip.setVisibility(View.GONE);
-        }
-        if (position == 0) {
+        if (holder instanceof ItemViewHolder) {
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+            final int itemPosition = position - 1;
+            itemHolder.tvTitle.setText(titleTexts[itemPosition]);
+            itemHolder.tvTitle.setCompoundDrawablesWithIntrinsicBounds(iconRes[itemPosition],
+                    0, 0, 0);
+            itemHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickPosition(position);
+                }
+            });
+            if (itemPosition >= titleTexts.length - 2) {
+                itemHolder.tvTip.setVisibility(View.VISIBLE);
+                if (itemPosition == titleTexts.length - 1) {
 
-            if (mUnreadCount > 0) {
-                holder.tvUnReadCount.setVisibility(View.VISIBLE);
-                holder.tvUnReadCount.setText(mUnreadCount + "");
+                    itemHolder.tvTip.setText(R.string.tip_flowpackage);
+                } else if (itemPosition == titleTexts.length - 2 && !TextUtils.isEmpty(mInviteCode)) {
+                    itemHolder.tvTip.setText(mContext.getString(R.string.tip_invite_code, mInviteCode));
+                }
             } else {
-                holder.tvUnReadCount.setVisibility(View.GONE);
+                itemHolder.tvTip.setVisibility(View.GONE);
             }
+            if (itemPosition == 0) {
 
-        } else {
-            holder.tvUnReadCount.setVisibility(View.GONE);
+                if (mUnreadCount > 0) {
+                    itemHolder.tvUnReadCount.setVisibility(View.VISIBLE);
+                    itemHolder.tvUnReadCount.setText(mUnreadCount + "");
+                } else {
+                    itemHolder.tvUnReadCount.setVisibility(View.GONE);
+                }
+
+            } else {
+                itemHolder.tvUnReadCount.setVisibility(View.GONE);
+            }
+        } else if (holder instanceof HeadViewHolder) {
+            HeadViewHolder headHolder = (HeadViewHolder) holder;
+            headHolder.updateView();
         }
+
+
     }
 
 
@@ -134,35 +173,35 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
         }
         switch (position) {
 
-            case 0: //消息中心
+            case 1: //消息中心
 
 
                 MessageManager.getInstance().startConversationList(mContext);
 
                 break;
-            case 1: //我的组合
+            case 2: //我的组合
 
                 UIUtils.startAnimationActivity((Activity) mContext, new Intent(mContext, MyCombinationActivity.class));
 
 
                 break;
 
-            case 2://我的话题
+            case 3://我的话题
 
                 UserEntity userEntity = UserEngineImpl.getUserEntity();
                 UserTopicsActivity.starActivity(mContext, userEntity.getId() + "", userEntity.getUsername());
                 break;
-            case 3://我的回复
+            case 4://我的回复
                 UIUtils.startAnimationActivity((Activity) mContext, ReplyActivity.getIntent(mContext, GlobalParams.LOGIN_USER.getId() + ""));
 
                 break;
-            case 4://我的草稿
+            case 5://我的草稿
                 UIUtils.startAnimationActivity((Activity) mContext, new Intent(mContext, MyDraftActivity.class));
 
                 break;
 
 
-            case 5://邀请好友
+            case 6://邀请好友
 
                 UIUtils.startAnimationActivity((Activity) mContext, new Intent(mContext, InviteFriendsActivity.class));
 
@@ -170,7 +209,7 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
                 break;
 
 
-            case 6://流量兑换
+            case 7://流量兑换
 
                 UIUtils.startAnimationActivity((Activity) mContext, new Intent(mContext, FlowPackageActivity.class));
 
@@ -180,16 +219,30 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return titleTexts.length;
+        return titleTexts.length + 1;
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+
 
     @Override
     public int dividerSize(int position, RecyclerView parent) {
         switch (position) {
             case 0:
             case 1:
-            case 4:
-            case 6:
+            case 2:
+            case 5:
+            case 7:
                 return parent.getResources().getDimensionPixelOffset(R.dimen.combin_horSpacing);
 
             default:
@@ -198,13 +251,13 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
     }
 
 
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvTitle;
         TextView tvUnReadCount;
         TextView tvTip;
 
-        public ViewHolder(View view) {
+        public ItemViewHolder(View view) {
             super(view);
             tvTitle = (TextView) view.findViewById(R.id.tv_infotitle);
             tvUnReadCount = (TextView) view.findViewById(R.id.tv_unread_count);
@@ -212,4 +265,135 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
         }
 
     }
+
+    static class HeadViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @ViewInject(R.id.ll_login_layout)
+        private View viewLogin;
+        @ViewInject(R.id.ll_userinfo_layout)
+        private View viewUserInfo;
+        @ViewInject(R.id.setting_image_head)
+        private ImageView settingImageHead;
+        @ViewInject(R.id.setting_text_account_text)
+        private TextView settingTextAccountText;
+        @ViewInject(R.id.setting_text_name_text)
+        private TextView settingTextNameText;
+//
+//    @ViewInject(R.id.tv_unread_count)
+//    private TextView unreadCountTV;
+
+        @ViewInject(R.id.tv_followers)
+        private TextView tvFollowers;
+
+        @ViewInject(R.id.tv_following)
+        private TextView tvFollowing;
+        private View mView;
+
+        public HeadViewHolder(View view) {
+            super(view);
+            this.mView = view;
+            ViewUtils.inject(this, view); // 注入view和事件
+        }
+
+        @OnClick({R.id.btn_login, R.id.setting_layout_icon, R.id.user_myfunds_layout, R.id.ll_following, R.id.ll_followers})
+        public void onClick(View v) {
+            int id = v.getId();
+
+            switch (id) {
+                case R.id.btn_login:
+                    UIUtils.iStartLoginActivity(mView.getContext());
+                    break;
+                case R.id.setting_layout_icon:
+                    startUserInfoActivity();
+                    break;
+                case R.id.user_myfunds_layout:
+                    if (!UIUtils.iStartLoginActivity(mView.getContext())) {
+                        UIUtils.startAnimationActivity((Activity) mView.getContext(), new Intent(mView.getContext(), MyCombinationActivity.class));
+                    }
+                    break;
+
+                case R.id.ll_following:
+                    Intent followIntent = new Intent(mView.getContext(), FriendsOrFollowersActivity.class);
+                    followIntent.putExtra(FriendsOrFollowersActivity.KEY, FriendsOrFollowersActivity.FRIENDS);
+                    followIntent.putExtra(FriendsOrFollowersActivity.USER_ID, UserEngineImpl.getUserEntity().getId() + "");
+                    UIUtils.startAnimationActivity((Activity) mView.getContext(), followIntent);
+                    break;
+                case R.id.ll_followers:
+                    Intent intent1 = new Intent(mView.getContext(), FriendsOrFollowersActivity.class);
+                    intent1.putExtra(FriendsOrFollowersActivity.KEY, FriendsOrFollowersActivity.FOLLOWER);
+                    intent1.putExtra(FriendsOrFollowersActivity.USER_ID, UserEngineImpl.getUserEntity().getId() + "");
+                    UIUtils.startAnimationActivity((Activity) mView.getContext(), intent1);
+                    break;
+
+            }
+
+
+        }
+
+
+        private void startUserInfoActivity() {
+            Intent intent = UserHomePageActivity.getIntent(mView.getContext(), UserEngineImpl.getUserEntity().getUsername(),
+                    UserEngineImpl.getUserEntity().getId() + "");
+            UIUtils.startAnimationActivity((Activity) mView.getContext(), intent);
+        }
+
+        public void updateView() {
+            if (PortfolioApplication.hasUserLogin()) {
+                viewLogin.setVisibility(View.GONE);
+                viewUserInfo.setVisibility(View.VISIBLE);
+                String account = PortfolioPreferenceManager.getStringValue(PortfolioPreferenceManager.KEY_USER_ACCOUNT);
+                if (!TextUtils.isEmpty(account)) {
+                    settingTextAccountText.setText(account);
+                }
+                settingTextNameText.setText(PortfolioPreferenceManager
+                        .getStringValue(PortfolioPreferenceManager.KEY_USERNAME));
+
+                String url = PortfolioPreferenceManager.getStringValue(PortfolioPreferenceManager.KEY_USER_HEADER_URL);
+                if (!TextUtils.isEmpty(url) && !TextUtils.isEmpty(GlobalParams.ACCESS_TOCKEN)) {
+                    BitmapUtils bitmapUtils = new BitmapUtils(mView.getContext());
+                    bitmapUtils.configDefaultLoadFailedImage(R.drawable.ic_user_head);
+                    bitmapUtils.display(settingImageHead, url);
+
+                } else {
+                    Bitmap b = BitmapFactory.decodeResource(mView.getResources(), R.drawable.ic_user_head);
+                    b = UIUtils.toRoundBitmap(b);
+                    settingImageHead.setImageBitmap(b);
+
+                }
+
+                UserEntity userEntity = UserEngineImpl.getUserEntity();
+                new UserEngineImpl().getBaseUserInfo(userEntity.getId() + "", userInfoListener);
+            } else {
+                viewLogin.setVisibility(View.VISIBLE);
+                viewUserInfo.setVisibility(View.GONE);
+            }
+
+
+        }
+
+
+        ParseHttpListener userInfoListener = new ParseHttpListener<UserEntity>() {
+
+            @Override
+            protected UserEntity parseDateTask(String jsonData) {
+
+                return DataParse.parseObjectJson(UserEntity.class, jsonData);
+            }
+
+            @Override
+            protected void afterParseData(UserEntity object) {
+                if (null != object) {
+                    updateUserFollowInfo(object);
+                }
+
+            }
+        };
+
+        private void updateUserFollowInfo(UserEntity object) {
+            tvFollowers.setText(StringFromatUtils.handleNumber(object.getFollowed_by_count()));
+            tvFollowing.setText(StringFromatUtils.handleNumber(object.getFriends_count()));
+        }
+
+    }
+
 }
