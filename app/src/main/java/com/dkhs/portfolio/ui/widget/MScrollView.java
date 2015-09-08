67bcb0821f90e736650499359bd3e4e23eca354e
曾伟明 <wyh405003836@126.com>
2015-09-08 18:08:56 +0800

    帖子Bug修复

--- app/src/main/java/com/dkhs/portfolio/ui/widget/MScrollView.java
+++ app/src/main/java/com/dkhs/portfolio/ui/widget/MScrollView.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 import com.dkhs.portfolio.R;
 import com.lidroid.xutils.util.LogUtils;
@@ -22,6 +23,7 @@ public class MScrollView extends NestedScrollView {
 
 
     private MListView mMListView;
+    private final int[ ] mScrollOffset = new int[2];
     public MScrollView(Context context) {
         super(context);
     }
@@ -38,7 +40,6 @@ public class MScrollView extends NestedScrollView {
     protected void onFinishInflate() {
         View view=   LayoutInflater.from(getContext()).inflate(R.layout.scrollview_contain_listview,this,false);
         mMListView= (MListView) view.findViewById(R.id.lv);
-
         addView(view);
 
         super.onFinishInflate();
@@ -53,7 +54,6 @@ public class MScrollView extends NestedScrollView {
             isFirst =false;
             ViewGroup.LayoutParams layoutParams=   mMListView.getLayoutParams();
             layoutParams.height=getMeasuredHeight();
-
             mMListView.requestLayout();
         }
 
@@ -61,25 +61,48 @@ public class MScrollView extends NestedScrollView {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-
-
-        LogUtils.e("onInterceptTouchEvent");
-        if(mMListView.getTop()-getScrollY()<=0){
-            this.setScrollY(mMListView.getTop());
-            return  false;
-        }
-
-        return super.onInterceptTouchEvent(ev);
+        return  false;
     }
 
     @Override
     public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
 
+
         super.onNestedScroll(target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
-        this.scrollBy(dxConsumed, dyConsumed);
-//        this.scrollBy(dxConsumed,dyConsumed);
-
-
-//
+        this.smoothScrollBy(dxConsumed, dyConsumed);
+        if(dyUnconsumed >=0){
+            //向上
+            if(mMListView.getTop()>getScrollY()){
+                mMListView.isBlock=true;
+            }else{
+                mMListView.isBlock=false;
+            }
+        }else {
+            //向下
+            if(mMListView.getChildAt(0).getTop()==0){
+                mMListView.isBlock=true;
+            }else{
+                mMListView.isBlock=false;
+            }
+        }
     }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        super.onLayout(changed, l, t, r, b);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
 }
