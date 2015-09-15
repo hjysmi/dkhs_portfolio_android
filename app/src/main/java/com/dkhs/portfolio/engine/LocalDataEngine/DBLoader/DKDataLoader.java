1e8b1478e1e9474d35a00fe5aefc05275f4873d4
zhoujunzhou <zhoujunzhou@dkhs.com>
2015-09-15 17:47:20 +0800

    删除注释代码

--- app/src/main/java/com/dkhs/portfolio/engine/LocalDataEngine/DBLoader/DKDataLoader.java
+++ app/src/main/java/com/dkhs/portfolio/engine/LocalDataEngine/DBLoader/DKDataLoader.java
@@ -3,8 +3,6 @@ package com.dkhs.portfolio.engine.LocalDataEngine.DBLoader;
 import android.content.Context;
 import android.database.Cursor;
 
-import com.lidroid.xutils.util.LogUtils;
-
 /**
  * Created by zjz on 2015/9/10.
  */
@@ -17,7 +15,6 @@ public class DKDataLoader extends SimpleCursorLoader {
 
     @Override
     public Cursor loadCursorBackgroud() {
-        LogUtils.e("DKDataLoader loadCursorBackgroud");
         if (null != cursorCreate)
             return cursorCreate;
         return null;
