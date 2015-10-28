ce812a1da577d53d2b172db31c9486ec178f5f59
Congmin Zhang <zhangcongmin@dkhs.com>
2015-10-28 15:16:51 +0800

    修改钱包部分api未加密请求

--- app/src/main/java/com/dkhs/portfolio/engine/WalletsEngine.java
+++ app/src/main/java/com/dkhs/portfolio/engine/WalletsEngine.java
@@ -6,6 +6,8 @@ import com.dkhs.portfolio.net.ParseHttpListener;
 import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.client.HttpRequest;
 
+import java.util.TreeMap;
+
 /**
  * Created by zjz on 2015/10/9.
  */
@@ -17,9 +19,10 @@ public class WalletsEngine {
 
     ////  vendor (string, 第三方支付渠道，weixinpay,微信／alipay,支付宝／yibao,易宝)
     public static void payment(float amount, String vendor, ParseHttpListener listener) {
-        RequestParams params = new RequestParams();
-        params.addBodyParameter("amount", amount + "");
-        params.addBodyParameter("vendor", vendor);
-        DKHSClient.request(HttpRequest.HttpMethod.POST, DKHSUrl.Wallets.payment, params, listener);
+        //加密请求
+        TreeMap<String, String> paramsMap = new TreeMap<>();
+        paramsMap.put("amount", String.valueOf(amount));
+        paramsMap.put("vendor", vendor);
+        DKHSClient.requestPostByEncryp(paramsMap, DKHSUrl.Wallets.payment, listener);
     }
 }
