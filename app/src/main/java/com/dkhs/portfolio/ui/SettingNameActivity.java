ad710e8b327de31fbacaac9b6417cf1c2d2eb5a5
wuyongsen <wuyongsen@dkhs.com>
2015-11-23 12:35:19 +0800

    修改注册－填写昵称页文字

--- app/src/main/java/com/dkhs/portfolio/ui/SettingNameActivity.java
+++ app/src/main/java/com/dkhs/portfolio/ui/SettingNameActivity.java
@@ -285,13 +285,13 @@ public class SettingNameActivity extends ModelAcitivity implements OnClickListen
             findViewById(R.id.rl_repassword).setVisibility(View.VISIBLE);
 
         } else if (isSetPsw) {
-            setTitle("设置密码");
+            setTitle("设置新密码");
             findViewById(R.id.ll_name).setVisibility(View.GONE);
             TextView tvPsw = (TextView) findViewById(R.id.tv_psw);
-            tvPsw.setText("密码");
+            tvPsw.setText("新密码");
 
         } else {
-            setTitle("设置昵称和密码");
+            setTitle("填写昵称");
             rlfbutton.setText("完成");
 
         }
