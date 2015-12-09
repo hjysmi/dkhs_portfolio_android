339b8d189332d8e258d379ed81fcd4668aa87db2
xuetong <xuetong@dkhs.com>
2015-12-09 11:09:50 +0800

    牛人招募之简介

--- app/src/main/java/com/dkhs/portfolio/ui/fragment/PersonalFragment.java
+++ app/src/main/java/com/dkhs/portfolio/ui/fragment/PersonalFragment.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.percent.PercentFrameLayout;
 import com.dkhs.portfolio.R;
 import com.dkhs.portfolio.ui.AgreementTextActivity;
+import com.dkhs.portfolio.ui.PersonalIntroduceActivity;
 
 /**
  * 牛人招募之个人资料
@@ -14,6 +16,9 @@ import com.dkhs.portfolio.ui.AgreementTextActivity;
  */
 public class PersonalFragment extends BaseFragment implements View.OnClickListener {
     private TextView rlt_agreement;
+    private PercentFrameLayout fm_introduce;
+    public static final int RESULT_INTRODUCE_BACK = 1;
+    private TextView tv_introduce;
 
     @Override
     public int setContentLayoutId() {
@@ -30,11 +35,13 @@ public class PersonalFragment extends BaseFragment implements View.OnClickListen
 
     private void initView(View view) {
         rlt_agreement = (TextView) view.findViewById(R.id.rlt_agreement);
-
+        fm_introduce = (PercentFrameLayout) view.findViewById(R.id.fm_introduce);
+        tv_introduce = (TextView) view.findViewById(R.id.tv_introduce);
     }
 
     private void initEvents() {
         rlt_agreement.setOnClickListener(this);
+        fm_introduce.setOnClickListener(this);
     }
 
     @Override
@@ -44,7 +51,29 @@ public class PersonalFragment extends BaseFragment implements View.OnClickListen
                 Intent intent = new Intent(getActivity(), AgreementTextActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.fm_introduce:
+                Intent intent_introduce = new Intent(getActivity(), PersonalIntroduceActivity.class);
+                startActivityForResult(intent_introduce, RESULT_INTRODUCE_BACK);
+                break;
         }
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case RESULT_INTRODUCE_BACK:
+                if (null == data) {
+                    return;
+                } else {
+                    String content = data.getStringExtra(PersonalIntroduceActivity.RESULT_CONTENT);
+                    tv_introduce.setText(content);
+                }
+                break;
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
