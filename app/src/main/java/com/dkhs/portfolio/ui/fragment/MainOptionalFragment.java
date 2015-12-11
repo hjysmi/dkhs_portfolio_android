373396e734e75730b09eaed12bc1a67b29104e4d
wuyongsen <wuyongsen@dkhs.com>
2015-12-11 11:04:17 +0800

    自选基金由本地搜索改为在线搜索

--- app/src/main/java/com/dkhs/portfolio/ui/fragment/MainOptionalFragment.java
+++ app/src/main/java/com/dkhs/portfolio/ui/fragment/MainOptionalFragment.java
@@ -24,7 +24,6 @@ import com.dkhs.portfolio.ui.EditTabCombinationActivity;
 import com.dkhs.portfolio.ui.EditTabFundActivity;
 import com.dkhs.portfolio.ui.EditTabStockActivity;
 import com.dkhs.portfolio.ui.MainActivity;
-import com.dkhs.portfolio.ui.SelectAddOptionalActivity;
 import com.dkhs.portfolio.ui.SelectGeneralActivity;
 import com.dkhs.portfolio.ui.adapter.BasePagerFragmentAdapter;
 import com.dkhs.portfolio.ui.eventbus.BusProvider;
@@ -269,8 +268,7 @@ public class MainOptionalFragment extends VisiableLoadFragment implements IDataU
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SelectAddOptionalActivity.class);
-//                Intent intent = new Intent(getActivity(), SelectGeneralActivity.class);
+                Intent intent = new Intent(getActivity(), SelectGeneralActivity.class);
                 UIUtils.startAnimationActivity(getActivity(), intent);
             }
         });
@@ -296,8 +294,7 @@ public class MainOptionalFragment extends VisiableLoadFragment implements IDataU
 
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SelectAddOptionalActivity.class);
-//                Intent intent = new Intent(getActivity(), SelectGeneralActivity.class);
+                Intent intent = new Intent(getActivity(), SelectGeneralActivity.class);
                 UIUtils.startAnimationActivity(getActivity(), intent);
             }
         });
