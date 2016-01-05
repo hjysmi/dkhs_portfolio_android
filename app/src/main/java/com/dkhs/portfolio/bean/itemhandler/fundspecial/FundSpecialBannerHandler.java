c3f3186a630199c431d22260732bfa12217323bc
Congmin Zhang <zhangcongmin@dkhs.com>
2016-01-05 16:19:18 +0800

    基金首页banner背景色块逻辑修改

--- app/src/main/java/com/dkhs/portfolio/bean/itemhandler/fundspecial/FundSpecialBannerHandler.java
+++ app/src/main/java/com/dkhs/portfolio/bean/itemhandler/fundspecial/FundSpecialBannerHandler.java
@@ -23,6 +23,8 @@ import com.dkhs.portfolio.utils.ImageLoaderUtils;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -38,9 +40,13 @@ public class FundSpecialBannerHandler extends SimpleItemHandler<Message> {
         int width = displayMetrics.widthPixels * 5 / 8 + mContext.getResources().getDimensionPixelOffset(R.dimen.widget_padding_normal);
         int height = width / 2;
         params = new RelativeLayout.LayoutParams(width, height);
+        colors = Arrays.asList(colorRandom);
+
     }
+
     private RelativeLayout.LayoutParams params;
     private static String[] colorRandom = new String[]{"#70aba0", "#e4b524", "#86b2f6", "#f77d7b", "#f4ad56", "#f9760b"};
+    private List<String> colors;
 
     @Override
     public int getLayoutResId() {
@@ -50,38 +56,40 @@ public class FundSpecialBannerHandler extends SimpleItemHandler<Message> {
     @Override
     public void onBindView(ViewHolder vh, final Message msg, int position) {
         final List<RecommendFundSpecialBannerBean> lists = (List<RecommendFundSpecialBannerBean>) msg.obj;
-        if(lists == null || lists.size() == 0){
+        if (lists == null || lists.size() == 0) {
             return;
         }
-        if(lists.get(0).isFirstTimeLoad){
+        if (lists.get(0).isFirstTimeLoad) {
+            Collections.shuffle(colors);
             LinearLayout ll = vh.get(R.id.ll_fund);
             ll.removeAllViews();
             View leftView = new View(mContext);
             leftView.setLayoutParams(new ViewGroup.LayoutParams(mContext.getResources().getDimensionPixelOffset(R.dimen.widget_margin_medium), 1));
             ll.addView(leftView);
-            for(final RecommendFundSpecialBannerBean item : lists){
-                View bannerItem =  View.inflate(mContext, R.layout.item_market_fund_banner, null);;
+            for (int i = 0; i < lists.size(); i++) {
+                final int index = i % colors.size();
+                final RecommendFundSpecialBannerBean item = lists.get(i);
+                View bannerItem = View.inflate(mContext, R.layout.item_market_fund_banner, null);
                 bannerItem.setLayoutParams(params);
                 final ImageView iv_mask = (ImageView) bannerItem.findViewById(R.id.iv_mask);
                 final ImageView imageView = (ImageView) bannerItem.findViewById(R.id.iv_bg);
-                if(TextUtils.isEmpty(item.getRecommend_image_sm())){
-                    if(item.isFirstTimeLoad){
-                        imageView.setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
+                if (TextUtils.isEmpty(item.getRecommend_image_sm())) {
+                    if (item.isFirstTimeLoad) {
+                        imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                         item.isFirstTimeLoad = false;
                     }
-                }else{
+                } else {
                     ImageLoaderUtils.loadImage(item.getRecommend_image_sm(), new ImageLoadingListener() {
                         @Override
                         public void onLoadingStarted(String s, View view) {
                             iv_mask.setVisibility(View.GONE);
-                            imageView.setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
+                            imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                         }
 
                         @Override
                         public void onLoadingFailed(String s, View view, FailReason failReason) {
                             iv_mask.setVisibility(View.GONE);
-                            imageView.setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
-
+                            imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                         }
 
                         @Override
@@ -92,20 +100,19 @@ public class FundSpecialBannerHandler extends SimpleItemHandler<Message> {
                                 imageView.setBackgroundDrawable(bitmapDrawable);
                             } else {
                                 iv_mask.setVisibility(View.GONE);
-                                imageView.setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
+                                imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                             }
                         }
 
                         @Override
                         public void onLoadingCancelled(String s, View view) {
                             iv_mask.setVisibility(View.GONE);
-                            imageView.setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
-
+                            imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                         }
                     });
                 }
-                if(TextUtils.isEmpty(item.getRecommend_image_sm()) && item.isFirstTimeLoad){
-                    bannerItem.findViewById(R.id.ll_content).setBackgroundColor(Color.parseColor(colorRandom[new Random().nextInt(colorRandom.length)]));
+                if (TextUtils.isEmpty(item.getRecommend_image_sm()) && item.isFirstTimeLoad) {
+                    imageView.setBackgroundColor(Color.parseColor(colors.get(index)));
                     item.isFirstTimeLoad = false;
                 }
                 TextView tv_title = (TextView) bannerItem.findViewById(R.id.tv_title);
