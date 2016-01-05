1bfb0824bee0050bd40a99226878781c9555eb97
Congmin Zhang <zhangcongmin@dkhs.com>
2016-01-05 18:06:59 +0800

    修复bug

--- app/src/main/java/com/dkhs/portfolio/ui/BankCardInfoActivity.java
+++ app/src/main/java/com/dkhs/portfolio/ui/BankCardInfoActivity.java
@@ -610,11 +610,13 @@ public class BankCardInfoActivity extends ModelAcitivity implements View.OnClick
             se.startTag("", "CpPay");
             se.attribute("", "application", "LunchPay.Req");
 
+            se.startTag("", "env");
             if (!TextUtils.isEmpty(bean.env)) {
-                se.startTag("", "env");
                 se.text(bean.env.toUpperCase());
-                se.endTag("", "env");
+            }else{
+                se.text("PRODUCT");
             }
+            se.endTag("", "env");
 
             se.startTag("", "merchantId");
             se.text(bean.merchantId);
